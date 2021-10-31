24dc93e9d26175c1c73f4ee15a99f87d867d537e
Drunk Fly <drunkflyteam@gmail.com>
2021-10-31 16:21:49 +0200

    Imported ezxml.

--- Tools/importer/compile.cmd
+++ Tools/importer/compile.cmd
@@ -1,5 +1,6 @@
 @echo off
-%~dp0..\tcc\tcc -I%~dp0..\..\Engine -o %~dp0importer.exe ^
+%~dp0..\tcc\tcc -I%~dp0..\..\Engine -DEZXML_NOMMAP -o %~dp0importer.exe ^
+    %~dp0ezxml/ezxml.c ^
     %~dp0image.c ^
     %~dp0main.c
 if errorlevel 1 exit /B 1
