f85e35c50f8937cc78aefe22e14f69d169e99a05
Drunk Fly <drunkflyteam@gmail.com>
2021-11-27 17:57:57 +0200

    Added MinGW support for PC build.

---
+++ build_mingw.cmd
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+if not exist %~dp0Build\CMake.MinGW mkdir %~dp0Build\CMake.MinGW
+if errorlevel 1 exit /B 1
+
+cd %~dp0Build\CMake.MinGW
+if errorlevel 1 exit /B 1
+
+if exist %~dp0Build\CMake.MinGW\CMakeCache.txt goto cmake_exists
+%~dp0Tools\cmake\bin\cmake ^
+    -G "MinGW Makefiles" ^
+    -DTARGET_SDL2=YES ^
+    %~dp0Build
+if errorlevel 1 exit /B 1
+:cmake_exists
+
+%~dp0Tools\cmake\bin\cmake --build . --config Release
+if errorlevel 1 exit /B 1
